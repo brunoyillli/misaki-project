9c1030f0586e9db4c93b44d78276b9210cb7a6da
brunoyillli <bruboneet@gmail.com>
2020-09-05 20:09:12 -0300

    ajuste na movimentacao, ataques basicos e limite da tela

---
+++ scripts/bullet_player.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+var vel = 300
+var dir = Vector2(0 , -1)
+
+func _ready():
+	add_to_group("bullet_initial")
+
+func _physics_process(delta):
+	translate(dir * vel * delta)
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
