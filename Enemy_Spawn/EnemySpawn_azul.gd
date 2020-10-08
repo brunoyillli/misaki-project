b5d47430b3426f2ee28e82b9bca9e6839c2377b1
JoaoPedro00 <JoaoPedro00@users.noreply.github.com>
2020-10-08 17:20:58 -0300

    Criação de 2 inimigos
    Criação de 2 inimigos porém um inimigo  não esta funcionando 100% no stage

--- Enemy_Spawn/EnemySpawn_azul.gd
+++ Enemy_Spawn/EnemySpawn_azul.gd
@@ -2,13 +2,16 @@ extends Node
 
 onready var enemy_scene = [load("res://Inimigos/inimigo_teleguiado.tscn")
 ]
+#onready var enemy_scene = [load("res://Inimigos/Inimigo_teleguiado_Grande/Inimigo_tele_Grande.tscn")
+#]
+
 var time = 0
 
 func _ready():
-	for i in range(5):
+	for i in range(3):
 		var enemy = enemy_scene[0].instance()
 		randomize()
-		enemy.position.x = rand_range(50, 910)
+		enemy.position.x = rand_range(60, 500)
 		enemy.position.y = -100
 		add_child(enemy)
 	
