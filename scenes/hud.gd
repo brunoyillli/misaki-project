3c89c1145da286c9ac70dce05c0269b875947acb
JoaoPedro00 <JoaoPedro00@users.noreply.github.com>
2020-10-16 20:56:43 -0300

    health hud

---
+++ scenes/hud.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+onready var player = get_node("../../player")
+
+const Hearth_row_size = 5
+const Hearth_offset = 8
+
+func _ready():
+	for i in player.max_health:
+		var new_heart = Sprite.new()
+		new_heart.texture = $hearts.texture
+		new_heart.hframes = $hearts.hframes
+		$hearts.add_child(new_heart)
+		
+	for heart in $hearts.get_children():
+		var index = heart.get_index()
+		
+		var x = (index % Hearth_row_size) * Hearth_offset
+		var y = (index / Hearth_row_size) * Hearth_offset
+		heart.position = Vector2(x, y)
+		
+func _process(delta):
+	for heart in $hearts.get_children():
+		var index = heart.get_index()
+		var last_heart = floor(player.health)
+		if index > last_heart:
+			heart.frame = 0
+		if index == last_heart:
+			heart.frame = (player.health - last_heart) * 4
+		if index < last_heart:
+			heart.frame = 4
