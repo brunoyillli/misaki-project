f96de4a5f6741d6e78ef94daab76c14267262d04
JoaoPedro00 <JoaoPedro00@users.noreply.github.com>
2020-10-25 14:19:38 -0300

    drops de inimigos ( vida e pontos)

---
+++ Cristais/Alma_Azul/alma_azul.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var vel = 300
+var dir = Vector2(0 , 1)
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	position.y += vel * delta
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+func _on_Alma_Azul_body_entered(body):
+	Global.points += 100
+	queue_free()
---
+++ Global.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var points = 0
+
+
+func instance_node(node, location, parent):
+	var node_instance = node.instance()
+	parent.add_child(node_instance)
+	node_instance.global_position = location
+	return node_instance
+	
